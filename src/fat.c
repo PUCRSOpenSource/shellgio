e1912218903a1447609e9511cc54cf1c51c46327
Daniel Amarante <daniel.amarante2@gmail.com>
2015-11-21 23:30:56 -0200

    adds end of string character to filename

--- src/fat.c
+++ src/fat.c
@@ -269,7 +269,7 @@ mkdir(char** path, int size)
 		if (cluster->dir[i].attributes != 1 && cluster->dir[i].attributes != 2)
 		{
 			int j;
-			for(j = 0; j < strlen(path[size - 1]); j++)
+			for(j = 0; j < strlen(path[size - 1]) + 1; j++)
 			{
 				cluster->dir[i].filename[j] = path[size - 1][j];
 			}
